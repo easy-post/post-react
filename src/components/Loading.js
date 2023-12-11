b50a4d02c485e20354c3ee022fc6bd40e7987db1
easy-post <kfs0502@naver.com>
2023-12-11 09:37:14 +0900

    Loading image

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,10 +2,11 @@ import React from "react";
 import '../scss/Loading.scss';
 
 const Loading = () => {
+  const EXPORT_HOST = "https://post-react.onrender.com";
   return (
     <div className="loading--wrap">
       <div className="loading--circle">
-        <img src={process.env.PUBLIC_URL + 'images/utility/loading.png'} alt="" />
+        <img src={EXPORT_HOST + '/images/utility/loading.png'} alt="" />
       </div>
     </div>
   );
