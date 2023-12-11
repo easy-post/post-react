73074d5e48356d04cc9ff46f0fadef3c917cd651
easy-post <kfs0502@naver.com>
2023-12-11 17:28:18 +0900

    server address

--- src/constants/ApiAddress.js
+++ src/constants/ApiAddress.js
@@ -1,6 +1,6 @@
 class ApiAdress{
   // static LOCAL_MEMBER="http://127.0.0.1:8080/member";
-  static LOCAL_MEMBER="https://easypostapp.com";
+  static LOCAL_MEMBER="https://easypostapp.com/member";
   static LOCAL_POST="https://easypostapp.com/post";
 }
 
