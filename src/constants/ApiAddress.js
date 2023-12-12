724bef6dbbc262777ee9c5204d869e8cbc7886d5
easy-post <kfs0502@naver.com>
2023-12-12 12:13:51 +0900

    cookie set in front

--- src/constants/ApiAddress.js
+++ src/constants/ApiAddress.js
@@ -3,6 +3,7 @@ class ApiAdress{
   static LOCAL_MEMBER="https://easypostapp.com/member";
   static LOCAL_POST="https://easypostapp.com/post";
   static LOCAL="https://easypostapp.com";
+  static LOCAL_COOKIE="easypostapp.com";
 }
 
 export default ApiAdress;
