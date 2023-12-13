34cc5448c7ed5339cf7137988035d32dde463354
easy-post <kfs0502@naver.com>
2023-12-13 17:10:24 +0900

    cookie set in front

--- src/pages/NewPost.js
+++ src/pages/NewPost.js
@@ -25,21 +25,22 @@ const NewPost = () => {
         break;
 
       default:
-        axios.get(`${ApiAdress.LOCAL}${location.pathname}`, {
-          withCredentials:true,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-        .then(res=>{
-          if(!res.status===200) throw new Error(res.data.message);
+        axios
+          .get(`${ApiAdress.LOCAL}${location.pathname}`, {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+          .then((res) => {
+            if (!res.status === 200) throw new Error(res.data.message);
 
-          $title.current.value = res.data.title;
-          $content.current.innerHTML = res.data.html;
-        })
-        .catch(err=>{
-          console.log(err);
-        })
+            $title.current.value = res.data.title;
+            $content.current.innerHTML = res.data.html;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
         break;
     }
   }, [checkLogin]);
@@ -51,143 +52,153 @@ const NewPost = () => {
     switch (location.pathname) {
       case "/post/new":
         axios
-        .post(
-          `${ApiAdress.LOCAL_POST}/save`,
-          {
-            title: e.target.title.value,
-            html: $content.current.innerHTML,
-          },
-          {
-            withCredentials: true,
-            headers: {
-              "Content-Type": "application/json",
+          .post(
+            `${ApiAdress.LOCAL_POST}/save`,
+            {
+              title: e.target.title.value,
+              html: $content.current.innerHTML,
             },
-          }
-        )
-        .then((res) => {
-          setIsSaving(false);
-          if (res.status !== 200) {
-            throw new Error(res.data);
-          } else {
-            navigate(`/post/${res.data.id}`);
-          }
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
+            {
+              withCredentials: true,
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          )
+          .then((res) => {
+            setIsSaving(false);
+            if (res.status !== 200) {
+              throw new Error(res.data);
+            } else {
+              navigate(`/post/${res.data.id}`);
+            }
+          })
+          .catch((err) => {
+            console.log(err.message);
+          });
         break;
 
       default:
-        axios.post(`${ApiAdress.LOCAL}${location.pathname}`,{
-          title:e.target.title.value,
-          html:$content.current.innerHTML
-        },{
-          withCredentials:true,
-          headers:{"Content-Type":"application/json"}
-        })
-        .then(res=>{
-          if(!res.status === 200) throw new Error(res.data.message);
+        axios
+          .post(
+            `${ApiAdress.LOCAL}${location.pathname}`,
+            {
+              title: e.target.title.value,
+              html: $content.current.innerHTML,
+            },
+            {
+              withCredentials: true,
+              headers: { "Content-Type": "application/json" },
+            }
+          )
+          .then((res) => {
+            if (!res.status === 200) throw new Error(res.data.message);
 
-          navigate(`/post/${res.data.id}`);
-        })
-        
+            navigate(`/post/${res.data.id}`);
+          });
+
         break;
-
-
-  };
-
-  const onPasteHandler = (e) => {
-    const clipboardData = e.clipboardData;
-    const file = clipboardData.files[0];
-
-    if (file !== undefined) {
-      e.preventDefault();
-      const type = file.type.split("/");
-
-      if (type[0] === "image") {
-        const $imgWrap = document.createElement("div");
-        $imgWrap.classList.add("img--wrap");
-
-        const $loading = document.createElement("img");
-        $loading.setAttribute(
-          "src",
-          `${EXPORT_HOST + "/images/utility/loading.png"}`
-        );
-        $loading.setAttribute("alt", "loading image");
-        $loading.classList.add("loading--img");
-        $imgWrap.appendChild($loading);
-
-        const selection = window.getSelection();
-        const range = selection.getRangeAt(0);
-        range.deleteContents();
-        range.insertNode($imgWrap);
-        const $emptyP = document.createElement("p");
-
-        $imgWrap.insertAdjacentElement("afterend", $emptyP);
-
-        const newRange = document.createRange();
-        newRange.setStartAfter($emptyP, 0);
-        newRange.collapse(true);
-        selection.removeAllRanges();
-        selection.addRange(newRange);
-
-        const fileReader = new FileReader();
-        fileReader.readAsDataURL(file);
-
-        fileReader.onload = (e) => {
-          axios
-            .post(
-              `${IMAGE_SERVER}/save`,
-              {
-                type: type[1],
-                file: e.target.result,
-              },
-              {
-                withCredentials: true,
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              }
-            )
-            .then((res) => {
-              if (!(res.status == 200)) throw new Error(res.data);
-              const $img = document.createElement("img");
-              $img.setAttribute("src", res.data);
-              $img.setAttribute("alt", "post picture");
-
-              $imgWrap.removeChild($loading);
-              $imgWrap.appendChild($img);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        };
-      }
     }
+
+    const onPasteHandler = (e) => {
+      const clipboardData = e.clipboardData;
+      const file = clipboardData.files[0];
+
+      if (file !== undefined) {
+        e.preventDefault();
+        const type = file.type.split("/");
+
+        if (type[0] === "image") {
+          const $imgWrap = document.createElement("div");
+          $imgWrap.classList.add("img--wrap");
+
+          const $loading = document.createElement("img");
+          $loading.setAttribute(
+            "src",
+            `${EXPORT_HOST + "/images/utility/loading.png"}`
+          );
+          $loading.setAttribute("alt", "loading image");
+          $loading.classList.add("loading--img");
+          $imgWrap.appendChild($loading);
+
+          const selection = window.getSelection();
+          const range = selection.getRangeAt(0);
+          range.deleteContents();
+          range.insertNode($imgWrap);
+          const $emptyP = document.createElement("p");
+
+          $imgWrap.insertAdjacentElement("afterend", $emptyP);
+
+          const newRange = document.createRange();
+          newRange.setStartAfter($emptyP, 0);
+          newRange.collapse(true);
+          selection.removeAllRanges();
+          selection.addRange(newRange);
+
+          const fileReader = new FileReader();
+          fileReader.readAsDataURL(file);
+
+          fileReader.onload = (e) => {
+            axios
+              .post(
+                `${IMAGE_SERVER}/save`,
+                {
+                  type: type[1],
+                  file: e.target.result,
+                },
+                {
+                  withCredentials: true,
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                }
+              )
+              .then((res) => {
+                if (!(res.status == 200)) throw new Error(res.data);
+                const $img = document.createElement("img");
+                $img.setAttribute("src", res.data);
+                $img.setAttribute("alt", "post picture");
+
+                $imgWrap.removeChild($loading);
+                $imgWrap.appendChild($img);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          };
+        }
+      }
+    };
+
+    return (
+      <div className="NewPost">
+        <CoverScreenLoading style={{ display: isSaving ? "flex" : "none" }}>
+          <p>저장 중...</p>
+        </CoverScreenLoading>
+        {isChecked ? (
+          <form onSubmit={savePost}>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              placeholder="제목"
+              ref={$title}
+            />
+            <div
+              className="content"
+              contentEditable="true"
+              onPaste={onPasteHandler}
+              ref={$content}
+            ></div>
+
+            <button type="submit">저장</button>
+          </form>
+        ) : (
+          <Loading />
+        )}
+      </div>
+    );
   };
-
-  return (
-    <div className="NewPost">
-      <CoverScreenLoading style={{ display: isSaving ? "flex" : "none" }}>
-        <p>저장 중...</p>
-      </CoverScreenLoading>
-      {isChecked ? (
-        <form onSubmit={savePost}>
-          <input type="text" name="title" id="title" placeholder="제목" ref={$title}/>
-          <div
-            className="content"
-            contentEditable="true"
-            onPaste={onPasteHandler}
-            ref={$content}
-          ></div>
-
-          <button type="submit">저장</button>
-        </form>
-      ) : (
-        <Loading />
-      )}
-    </div>
-  );
 };
 
 export default NewPost;
